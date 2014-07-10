a494bfecfe8e50a69faa034d112c1f4ce0b1814a
Henry Catalini Smith <henry@catalinismith.se>
2014-07-10 18:18:33 +0100

    Switch off all the map controls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   var mapOptions = {
     center: new google.maps.LatLng(0, 0),
-    zoom: 3
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true,
+    draggable: false,
+    scrollwheel: false,
+    zoom: 3,
+    zoomControl: false
   };
   var worldBounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(0, -110),
