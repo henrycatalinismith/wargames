1ddba74b8863bc87b4d33d4507264d7765991306
Henry Catalini Smith <henry@catalinismith.se>
2014-07-14 19:17:22 +0100

    Install marionette

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 require.config({
 
-  paths : {
+  paths: {
     async: '/bower_components/requirejs-plugins/src/async',
     backbone: '/bower_components/backbone/backbone',
     googlemaps: '/bower_components/googlemaps-amd/src/googlemaps',
     jquery: '/bower_components/jquery/dist/jquery',
+    marionette: '/bower_components/marionette/lib/backbone.marionette',
     underscore: '/bower_components/underscore/underscore'
   },
 
   googlemaps: {
     key: 'AIzaSyDqRRPaAp8g_m5aOVqJpwMkrU7wV3yyhR0',
     libraries: 'geometry'
+  },
+
+  shim: {
+    jquery:     { exports: 'jQuery' },
+    underscore: { exports: '_' },
+    backbone:   { exports: 'Backbone', deps: ['jquery', 'underscore'] },
+    marionette: { exports: 'Marionette', deps: ['jquery', 'underscore', 'backbone'] }
   }
 
 });
