b506d973453099bf037ec07b7129c66a78d202ca
Henry Catalini Smith <henry@catalinismith.se>
2014-07-14 19:34:08 +0100

    Delete placeholder code
    This was the copy/paste code I found on StackOverflow when I was just finding
    out how feasible it was to simulate global thermonuclear war using Google Maps.
    Not needed now that the real building has begun.

--- src/index.js
+++ src/index.js
@@ -24,58 +24,5 @@ require.config({
 });
 
 define(['thermonuclearwar'], function(ThermonuclearWar) {
-
   ThermonuclearWar.start();
-  return;
-
-
-  var worldBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(0, -110),
-    new google.maps.LatLng(0, 110)
-  );
-
-  //map.fitBounds(worldBounds);
-
-  var dept_lat = 55.749792;
-  var dept_lng = 37.632495;
-  var arr_lat = 38.8935965;
-  var arr_lng = -77.014576;
-
-  var departure = new google.maps.LatLng(dept_lat, dept_lng); //Set to whatever lat/lng you need for your departure location
- var arrival = new google.maps.LatLng(arr_lat, arr_lng); //Set to whatever lat/lng you need for your arrival location
-  var line = new google.maps.Polyline({
-      path: [departure, departure],
-      strokeColor: "#FF0000",
-      strokeOpacity: 1,
-      strokeWeight: 1,
-      geodesic: true, //set to false if you want straight line instead of arc
-      map: map,
- });
- var step = 0;
- var numSteps = 250; //Change this to set animation resolution
- var timePerStep = 5; //Change this to alter animation speed
- var interval = setInterval(function() {
-     step += 1;
-     if (step > numSteps) {
-         clearInterval(interval);
-         explode();
-     } else {
-         var are_we_there_yet = google.maps.geometry.spherical.interpolate(departure,arrival,step/numSteps);
-         line.setPath([departure, are_we_there_yet]);
-     }
- }, timePerStep);
-
-  function explode() {
-    var circle = new google.maps.Circle({
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
-      map: map,
-      center: arrival,
-      radius: 100000
-    });
-  }
-
 });
