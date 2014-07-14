93a46e4da7b12e25f751e0527385db4030bc6176
Henry Catalini Smith <henry@catalinismith.se>
2014-07-14 21:03:37 +0100

    Reintroduce some control over the map

--- src/ui/view/map.js
+++ src/ui/view/map.js
@@ -7,8 +7,8 @@ define(['backbone', 'googlemaps!'], function(Backbone) {
         center: new google.maps.LatLng(55.3617609, -3.4433238),
         disableDefaultUI: true,
         disableDoubleClickZoom: true,
-        draggable: false,
-        scrollwheel: false,
+        draggable: true,
+        scrollwheel: true,
         zoom: 3,
         zoomControl: false
       };
