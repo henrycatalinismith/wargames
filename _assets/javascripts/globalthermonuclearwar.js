b5349cbe299f9332c6d43f2e7865ab12fce69524
Henry Catalini Smith <henry@catalinismith.se>
2014-08-25 14:05:22 +0100

    Delete old code

--- _assets/javascripts/globalthermonuclearwar.js
+++ _assets/javascripts/globalthermonuclearwar.js
@@ -45,31 +45,7 @@ $(document).ready(function() {
 
   tick(10);
 
-  var origin = new google.maps.LatLng(55.749792, 37.632495);
-  var target = new google.maps.LatLng(38.8935965, -77.014576);
-  var location = origin;
-
-  var heading = google.maps.geometry.spherical.computeHeading(origin, target);
-
-  var line = new google.maps.Polyline({
-    geodesic: true,
-      map: map.map,
-      path: [origin, location],
-      strokeColor: "#FF0000",
-      strokeOpacity: 1,
-      strokeWeight: 1
-  });
-
-  var tickLength = 10;
   function move() {
-    return;
-    var speed = 1000;
-    var elapsed = tickLength;
-    var distance = speed * elapsed;
-    var heading = google.maps.geometry.spherical.computeHeading(location, target);
-    location = google.maps.geometry.spherical.computeOffset(location, distance, heading);
-    line.setPath([origin, location]);
-
     var remaining = google.maps.geometry.spherical.computeDistanceBetween(location, target);
     if (remaining > 10000) {
       setTimeout(move, tickLength);
@@ -84,13 +60,6 @@ $(document).ready(function() {
         strokeOpacity: 0.8,
         strokeWeight: 2,
       });
-
-      setTimeout(function() {
-        line.setMap(null);
-      }, 1000);
     }
   }
-
-  move();
-
 });
