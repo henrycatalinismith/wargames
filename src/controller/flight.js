08e28f6353833505ab2ed43bc881caf26579a1b8
Henry Catalini Smith <henry@catalinismith.se>
2014-08-26 20:59:06 +0100

    Move flight code out of model

--- src/controller/flight.js
+++ src/controller/flight.js
@@ -1,13 +1,27 @@
 GlobalThermonuclearWar.Controller.Flight = Marionette.Controller.extend({
 
   initialize: function(options) {
+    var that = this;
     function tick(length) {
       options.missiles
         .filter(function(missile) { return missile.get('status') === 'flight'; })
-        .map(function(missile) { missile.tick(); });
+        .map(function(missile) { that.move.apply(missile); });
       setTimeout(tick, length);
     }
     tick(100);
+  },
+
+  move: function() {
+    var tickLength = 10;
+    var speed = 1000;
+    var elapsed = tickLength;
+    var distance = speed * elapsed;
+    var origin = new google.maps.LatLng(this.get('origin')[0], this.get('origin')[1]);
+    var target = new google.maps.LatLng(this.get('target')[0], this.get('target')[1]);
+    var location = new google.maps.LatLng(this.get('location')[0], this.get('location')[1]);
+    var heading = google.maps.geometry.spherical.computeHeading(location, target);
+    location = google.maps.geometry.spherical.computeOffset(location, distance, heading);
+    this.set('location', [location.lat(), location.lng()]);
   }
 
 });
--- src/model/missile.js
+++ src/model/missile.js
@@ -11,20 +11,6 @@ GlobalThermonuclearWar.Model.Missile = Backbone.Model.extend({
   detonate: function() {
     this.set('status', 'detonated');
     this.trigger('detonation', this);
-  },
-
-  tick: function() {
-    var tickLength = 10;
-    var speed = 1000;
-    var elapsed = tickLength;
-    var distance = speed * elapsed;
-    var origin = new google.maps.LatLng(this.get('origin')[0], this.get('origin')[1]);
-    var target = new google.maps.LatLng(this.get('target')[0], this.get('target')[1]);
-    var location = new google.maps.LatLng(this.get('location')[0], this.get('location')[1]);
-    var heading = google.maps.geometry.spherical.computeHeading(location, target);
-
-    location = google.maps.geometry.spherical.computeOffset(location, distance, heading);
-    this.set('location', [location.lat(), location.lng()]);
   }
 
 });
