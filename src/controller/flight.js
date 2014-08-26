41d200d49dfc1d214b390c2173b29d894c4c156a
Henry Catalini Smith <henry@catalinismith.se>
2014-08-26 20:20:36 +0100

    Fix terrible efficiency of this code

--- src/controller/flight.js
+++ src/controller/flight.js
@@ -1,16 +1,13 @@
 GlobalThermonuclearWar.Controller.Flight = Marionette.Controller.extend({
 
   initialize: function(options) {
-    this.missiles = options.missiles;
-    this.timeout = null;
-    this.tick();
-  },
-
-  tick: function() {
-    this.missiles
-      .filter(function(missile) { return missile.get('status') === 'flight'; })
-      .map(function(missile) { missile.tick(); });
-    this.timeout = setTimeout(this.tick.bind(this), 100);
+    function tick(length) {
+      options.missiles
+        .filter(function(missile) { return missile.get('status') === 'flight'; })
+        .map(function(missile) { missile.tick(); });
+      setTimeout(tick, length);
+    }
+    tick(100);
   }
 
 });
