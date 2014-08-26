b76b31cb65935fc10e937a37841a7e4dbabe0522
Henry Catalini Smith <henry@catalinismith.se>
2014-08-26 23:20:23 +0100

    Add player tracking

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var server = require('http').Server();
+var Guid = require('guid');
 var io = require('socket.io')(server);
+var _ = require('underscore');
+
+var players = {};
+
 io.on('connection', function(socket){
 
+  var player = {
+    id: Guid.create().value
+  };
+
+  players[player.id] = player;
+
+  _.values(players)
+    .filter(function(player) { return typeof player.latitude !== 'undefined'; })
+    .map(function(player) { socket.emit('player:located', player); });
+
+  socket.on('player:located', function(data) {
+    data.id = player.id;
+    players[player.id] = data;
+    socket.emit('player:located', data);
+    socket.broadcast.emit('player:located', data);
+  });
+
   socket.on('launch', function(data) {
+    data.playerId = player.id;
     socket.broadcast.emit('launch', data);
   });
 
   socket.on('disconnect', function() {
+    socket.broadcast.emit('player:left', player.id);
+    delete players[player.id];
   });
 
 });
