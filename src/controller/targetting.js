c423d587938a138fc38cb22209e7609ab3d3f273
Henry Catalini Smith <henry@catalinismith.se>
2014-08-27 22:07:07 +0100

    Ratelimit people to one shot per second

--- src/controller/targetting.js
+++ src/controller/targetting.js
@@ -4,13 +4,18 @@ GlobalThermonuclearWar.Controller.Targetting = Marionette.Controller.extend({
     this.map = options.map;
     this.missiles = options.missiles;
     this.player = options.player;
+    this.waiting = false;
 
-    google.maps.event.addListener(this.map.map, 'click', this.trackLaunch.bind(this));
-    google.maps.event.addListener(this.map.map, 'click', this.launchMissile.bind(this));
+    google.maps.event.addListener(this.map.map, 'click', this.handleClick.bind(this));
   },
 
-  trackLaunch: function(event) {
-    ga('send', 'event', 'missile', 'launch');
+  handleClick: function(event) {
+    var that = this;
+    if (!this.waiting) {
+      this.launchMissile(event);
+      setTimeout(function() { that.waiting = false; }, 1000);
+      this.waiting = true;
+    }
   },
 
   launchMissile: function(event) {
@@ -18,8 +23,13 @@ GlobalThermonuclearWar.Controller.Targetting = Marionette.Controller.extend({
       origin: [this.player.get('latitude'), this.player.get('longitude')],
       target: [event.latLng.lat(), event.latLng.lng()]
     });
+    this.trackLaunch(event);
     this.missiles.push(missile);
     missile.launch();
+  },
+
+  trackLaunch: function(event) {
+    ga('send', 'event', 'missile', 'launch');
   }
 
 });
