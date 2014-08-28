9af687a5f50bcda44901703cfe8660bd41bbdb1f
Henry Catalini Smith <henry@catalinismith.se>
2014-08-28 01:10:02 +0100

    Hide the player counter after about 10 seconds
    It's not very accurate for much longer than that.

--- src/view/bar.js
+++ src/view/bar.js
@@ -1,7 +1,13 @@
 GlobalThermonuclearWar.View.Bar = Backbone.View.extend({
 
   initialize: function(options) {
+    this.$playerCountText = this.$('#playerCountText');
     this.$playerCount = this.$('.playerCountNumber');
+    setTimeout(this.hidePlayerCount.bind(this), 10000);
+  },
+
+  hidePlayerCount: function(count) {
+    this.$playerCountText.fadeOut(5000);
   },
 
   updatePlayerCount: function(count) {
