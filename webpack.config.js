9d8e543e97a2e4f8210b62a72f6cf1ee96a3dfeb
Henry Catalini Smith <henry@catalinismith.se>
2020-11-09 10:14:50 +0100

    code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,46 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
 const path = require("path")
 
+const devServer = {}
+devServer.contentBase == __dirname
+devServer.compress = true
+devServer.port = 8080
+
+const entry = {}
+entry.three = "three"
+entry.wargames = {}
+entry.wargames.import = "./index.js"
+entry.wargames.dependOn = "three"
+
+const module_ = {}
+module_.rules = []
+module_.rules.push({
+	test: /\.(js)$/,
+	exclude: /node_modules/,
+	use: ["babel-loader"]
+})
+
+const output = {}
+output.filename = "[name].bundle.js"
+output.path = __dirname
+
+const plugins = []
+// plugins.push(new BundleAnalyzerPlugin)
+
+const resolve = {}
+resolve.extensions = []
+resolve.extensions.push("*")
+resolve.extensions.push(".js")
+
+const watch = true
+
 module.exports = {
-	devServer: {
-    contentBase: __dirname,
-    compress: true,
-    port: 8080,
-  },
-
-  entry: "./index.js",
-
-	module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-
-  output: {
-    filename: "wargames.js",
-    path: __dirname,
-  },
-
-  resolve: {
-    extensions: ["*", ".js"]
-  },
-
-	watch: true,
+	devServer,
+  entry,
+	module: module_,
+  output,
+	plugins,
+  resolve,
+	watch,
 }
 
