56f320909feaa52aa9586db34b6305d23263cd24
Henry Catalini Smith <henry@catalinismith.se>
2020-11-11 11:54:33 +0100

    bring back the old backend

--- src/joshua.js
+++ src/joshua.js
@@ -1,141 +1,45 @@
-const fs = require("fs")
-const http = require("http")
-const uuid = require("uuid")
+var server = require('http').Server();
+var Guid = require('guid');
+var io = require('socket.io')(server);
+var _ = require('underscore');
 
-try {
-  fs.symlinkSync(__dirname, `${__dirname}/pages`)
-} catch (e) {
-  // probably just already exists lololol
-}
+var players = {};
 
-const express = require("express")()
-const next = require("next")({
-  dev: true,
-  dir: __dirname,
-})
+io.on('connection', function(socket){
+  socket.emit('initialize:opponents', players);
 
-const server = http.createServer(express)
-const io = require("socket.io")(server)
+  var player = {
+    id: Guid.create().value
+  };
 
-const action = (type, args = []) => ({
-  [type]: (...argv) => ({
-    type,
-    ...argv.reduce((acc, curr) => {
-      acc[args[Object.values(acc).length]] = curr
-      return acc
-    }, {})
-  })
-})
+  players[player.id] = player;
+  socket.emit('initialize:player', player);
+  socket.broadcast.emit('broadcast:entrance', player);
 
-const before = (actionType, handler) => store => next => action => {
-  if (action.type.match(actionType)) {
-    return Promise
-      .resolve(handler.call(null, store, action))
-      .then(ret => ret !== false && next(action))
+  socket.on('report:location', function(data) {
+    //log(socket.request.socket.remoteAddress, 'report:location', data);
+    data.id = player.id;
+    players[player.id] = data;
+    socket.broadcast.emit('broadcast:location', data);
+  });
+
+  var broadcastLaunch = function(data) {
+    //log(socket.request.socket.remoteAddress, 'broadcast:launch', data);
+    data.playerId = player.id;
+    socket.broadcast.emit('broadcast:launch', data);
+    socket.broadcast.emit('launch', data);
   }
-  return next(action)
-}
 
-const reducer = (initialState, actions) => (state, action) => {
-  switch (true) {
-    case state === undefined:
-      return initialState
-    case actions.hasOwnProperty(action.type):
-      return actions[action.type](state, action)
-    default:
-      return state
-  }
-}
+  socket.on('report:launch', broadcastLaunch);
+  socket.on('launch', broadcastLaunch);
 
-const after = (actionType, handler) => store => next => action => {
-  const result = next(action)
-  if (action.type.match(actionType)) {
-    handler.call(null, store, action)
-  }
-  return result
-}
+  socket.on('disconnect', function() {
+    //log(socket.request.socket.remoteAddress, 'disconnect', null);
+    socket.broadcast.emit('broadcast:exit', player.id);
+    delete players[player.id];
+  });
 
-const insert = entityName => (entityState, action) => ({
-  ...entityState,
-  [action[entityName].id]: action[entityName],
-})
+});
 
-const replace = entityName => (entityState, action) => action[entityName]
-
-const update = entityName => (entityState, action) => ({
-  ...entityState,
-  ...action[entityName],
-})
-
-const remove = entityName => (entityState, action) => {
-  const { [action[entityName].id]: deleted, ...remaining } = entityState
-  return remaining
-}
-
-const fifo = entityName => ([forgotten, ...remaining], action) => [
-  ...remaining,
-  action[entityName],
-]
-
-const redux = require("redux")
-
-const actions = {
-  ...action("listen", ["port"]),
-  ...action("connect", ["player"]),
-  ...action("disconnect", ["player"]),
-  ...action("target", ["player", "origin", "target"]),
-  ...action("launch", ["player", "origin", "target"]),
-}
-
-
-const reducers = redux.combineReducers({
-  players: reducer({}, {
-    connect: insert("player"),
-    disconnect: remove("player"),
-  }),
-})
-
-const middlewares = redux.applyMiddleware.apply(null, [
-  after(/.*/, (store, action) => {
-    console.log([
-      `[${(new Date).toISOString()}]`,
-      `${action.type}`,
-    ].join(" "))
-  }),
-  
-  before("listen", (store, { port} ) => new Promise(resolve => {
-    server.listen(port, () => resolve())
-  })),
-  
-  after("target", (store, action) => {
-    store.dispatch(actions.launch(action.player, action.origin, action.target))
-  }),
-  
-  after("launch", (store, action) => {
-    io.emit("launch", action)
-  })
-])
-
-const store = redux.createStore(reducers, {}, middlewares)
-
-io.on("connection", socket => {
-  const id = uuid()
-  const player = { id }
-  store.dispatch(actions.connect(player))
-
-  socket.on("target", data => {
-    store.dispatch(actions.target(player, data.origin, data.target))
-  })
-  
-  socket.on("disconnect", () => {
-    store.dispatch(actions.disconnect(player))
-  })
-})
-
-express.get("/", (req, res) => next.render(req, res, "/falken"))
-express.use("/", next.getRequestHandler())
-
-next.prepare().then(() => {
-  store.dispatch(actions.listen(process.env.PORT || 3000))
-})
+module.exports = server;
 
